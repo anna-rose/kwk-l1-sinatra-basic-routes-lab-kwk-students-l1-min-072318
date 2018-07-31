13cef6691d25c741e7faed689afab3d780cfc5b5
anna-rose <917566@isd624.org>
2018-07-31 16:06:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end 
+  get '/name'
+  "My name is Anna."
+  end 
+  get '/hometown'
+  "My hometown is WBL."
+  end 
+  get '/favorite-song'
+  "My favorite song is IDK."
+  end 
 end
+
